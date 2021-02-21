54f9c2fd2e40191ec3e20cb108363eded9ad6b72
mrkamran104 <mrkamran104@gmail.com>
2021-02-21 12:46:34 +0500

    simple form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form, Formik } from "formik";
+import React from "react";
+import "./App.css";
+import * as Yup from "yup";
+import TextFieldUI from "./features/TextFieldUI";
+import { Button, Paper } from "@material-ui/core";
+
+interface submit {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const initialValues = { firstName: "", lastName: "", email: "", password: "" };
+
+const schema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(3, "Must be above 3 characters long")
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(3, "Must be above 3 characters long")
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  password: Yup.string()
+    .min(6, "Must be above 6 characters long")
+    .max(15, "Must be 15 characters or less")
+    .required("Required"),
+});
 
 function App() {
+  const forSubmit = (values: submit): void => {
+    alert(JSON.stringify(values, null, 2));
+    // console.log(values);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Paper className="paperStyle" elevation={3}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={forSubmit}
+          validationSchema={schema}
         >
-          Learn React
-        </a>
-      </header>
+          {({ dirty, isValid }) => (
+            <Form>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <TextFieldUI label="First Name" name="firstName" />
+                <TextFieldUI label="Last Name" name="lastName" />
+              </div>
+              <TextFieldUI label="Email" name="email" />
+              <TextFieldUI label="Password" name="password" />
+              <Button
+                disabled={!dirty || !isValid}
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Paper>
     </div>
   );
 }
---
+++ src/features/TextFieldUI.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles, TextField } from "@material-ui/core";
+import { Field, ErrorMessage } from "formik";
+
+interface props {
+  label: string;
+  name: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  helperText: {
+    margin: theme.spacing(1), //4 etc
+    color: "red",
+  },
+  //   error: {
+  //     "&.MuiFormHelperText-root.Mui-error": {
+  //       color: theme.palette.common.white,
+  //     },
+  //   },
+}));
+
+const TextFieldUI: React.FC<props> = ({ label, name }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Field
+        as={TextField}
+        fullwidth="true"
+        required
+        name={name}
+        label={label}
+        variant="outlined"
+        helperText={<ErrorMessage name={name} />}
+        FormHelperTextProps={{ className: classes.helperText }}
+      />
+      {/* <TextField label="Outlined" variant="outlined" /> */}
+    </div>
+  );
+};
+
+export default TextFieldUI;
